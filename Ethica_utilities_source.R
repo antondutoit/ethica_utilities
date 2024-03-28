f31b4f5622aaa15918f945c0964c7e4497345611
benifex <atdutoit@gmail.com>
2024-03-28 23:45:39 +1100

    dynamic assignment of variable names

---
+++ Ethica_utilities_source.R
@@ -0,0 +1,3 @@
+# Title:    Ethica_utilities_source
+# Purpose:  Container for Ethica_utilities functions and 
+
--- testing.R
+++ testing.R
@@ -27,5 +27,72 @@ str_remove_all(tvec, c("a","c"))
 str_c("opt_", tvec)
 str_c(tvec, "opt_")   # in the order of the arguments, unsurprisingly 
 
-# test 5 
+# test 5 - using assign() and := to dynamically create variables ----- 
+# example code from https://stackoverflow.com/questions/26003574/use-dynamic-name-for-new-column-variable-in-dplyr 
+# In the new release of dplyr (0.6.0 awaiting in April 2017), we can also do an assignment (:=) and pass variables as column names by unquoting (!!) to not evaluate it
+# 
+# library(dplyr)
+# multipetal <- function(df, n) {
+#   mutate(df, "petal.{n}" := Petal.Width * n)
+# }
+
+# this works, as far as it goes ------
+test_df <- data.frame(a = c(6,7,8), b = c("a", "b", "c"))
+j <- 1
+  varname <- paste("constant_val", j, sep = "." )
+  assign(varname, 1:nrow(test_df))
+
+test_df %<>% mutate("constant_val.{j}" := eval(sym(varname)))
+
+# 2nd iteration: loop it ------
+test_df <- data.frame(a = c(6,7,8), b = c("a", "b", "c"))
+j <- 0
+for (j in 0:2)  {
+  j <- j + 1
+
+  varname <- paste("constant_val", j, sep = "." )
+  assign(varname, 1:nrow(test_df))
+
+  test_df %<>% mutate("constant_val.{j}" := eval(sym(varname)))
+  }
+
+
+# checks -----
+varname             # [1] "constant_val.1"
+eval(varname)       # [1] "constant_val.1"
+eval(sym(varname))  # [1] 1 2 3
+constant_val.1      # [1] 1 2 3
+
+# example code from online docs for assign() -----
+for(i in 1:6) { #-- Create objects  'r.1', 'r.2', ... 'r.6' --
+  nam <- paste("r", i, sep = ".")
+  assign(nam, 1:i)
+}
+
+ls(pattern = "^r..$")
+nam
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

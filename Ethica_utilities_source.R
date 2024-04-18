89bc63b808dc5a1e1d4c3219baaa0073df1bbd07
antondutoit <atdutoit@gmail.com>
2024-04-18 15:30:34 +1000

    creation of v0.3 Rmd as multiple functions now in source script. Rmd is now strictly a workflow. Remainder of deprecated code moved into deprecated.R

--- Ethica_utilities_source.R
+++ Ethica_utilities_source.R
@@ -51,5 +51,145 @@ mk <- function(x) {
   kable(x) %>%kableExtra::kable_styling(bootstrap_options = c("striped", "hover", "condensed"))
 }
 
-# chunk:  -----
+# Functions  -----
+# 1. Was: {r Create: functions for data frame naming, warning=FALSE, message=FALSE}
+import_CSV <<- function() {
+  # Load and rename CSV files 
+  file_names <<- Sys.glob(paste0(data_path, "/*.csv"))
+  
+  # Set up empty objects 
+  df_names <<- data.frame()
+  activity_numbers <<- vector()
+  df_list <<- list()
+  
+  # extract study number
+  study_number <<- str_extract(file_names[1], pattern = pattern1)
+  study_number <<- study_number %>% str_remove_all("activity_response_")
+  
+  # Create data frame names from file_names and read in data from CSV files 
+  i <- 0   
+  for (file_name in file_names) {
+    i <- i + 1
+    # extract activity number
+    activity_numbers[i] <<- str_extract(file_name, pattern = pattern3 )
+    activity_numbers[i] <<- activity_numbers[i] %>% str_remove_all(pattern3.1) %>% str_remove_all("_")
+    
+    # Generate df names from CSV file names
+    df_name <<- paste0("S", study_number, "_", activity_numbers[i], "_df", collapse = NULL)
+    
+    df_names <<- c(df_names, df_name)
+    df_names <<- as.vector(df_names, mode = "character")
+    
+    # read in CSV files   
+    df_list[[df_name]] <<- read_csv(file_name, col_names = TRUE, na = c("", "NA"))
+    
+    # how to make all these objects available in the env ?
+    # return(list(obj1, obj2))
+  }
+}
+
+# 2. Was: {r Create:   functions to clean column names, detect and remove columns with answer labels}
+clean_all_names <<- function() {
+  for (df_name in df_names)  {
+    df_list[[df_name]] <<- df_list[[df_name]] %>% janitor::clean_names()
+  }
+}
+
+# 3. Was: {r Create:   functions to clean column names, detect and remove columns with answer labels}
+remove_answ_cols <<- function() {
+  for (df_name in df_names)  {
+    df_list[[df_name]] <<- df_list[[df_name]] %>% select(!matches(pattern2))
+    df_list[[df_name]] <<- df_list[[df_name]] %>% select(!matches(pattern2.1))
+  }
+}
+
+# 4. Was: {r Fix:    extract numeric answer codes and rewrite 'metadata' answer columns}
+extract_saq_numbers <- function() {
+  for (df_name in df_names)  {
+    df_list[[df_name]] <<- df_list[[df_name]] %>% mutate(across(contains("_metadata_"), number_extract_single2))
+  }
+}
+
+# 5. Was: {r Fix:    rename answer columns}
+rename_answ_cols <- function() {
+  i <- 0
+  for (df_name in df_names)  {
+    i <- i + 1
+    
+    column_names <<- colnames(df_list[[df_name]])
+    logV <<- str_detect(column_names, pattern = pattern5)
+    activity_no <<- str_extract(df_name, pattern = pattern6) %>% str_remove_all("df") %>% str_remove_all("_")
+    
+    raw_question_numbers_int <<- str_extract(column_names[logV], pattern = pattern5)
+    raw_question_numbers <<- str_extract(raw_question_numbers_int, pattern = pattern5.1)
+    raw_question_numbers <<-  raw_question_numbers %>% str_remove("x")
+    
+    # extract 3 letter code for question type
+    question_type_int <<- str_extract(column_names[logV], pattern = pattern7)    
+    question_type <<- str_extract(question_type_int, pattern = pattern7.1)
+    
+    maqV <<- str_detect(column_names[logV], pattern = "_maq_")
+    
+    if (sum(maqV)>0) {
+      maq_option_no <<- str_extract(column_names[logV], pattern =  pattern8) %>% str_remove("_maq") %>% str_remove("_") %>%  str_replace_na(replacement = "NA") %>% str_replace("NA", "") 
+      maq_option_no <<- maq_option_no %>%  str_replace_all(capture(repeated(DGT,1,2)), "_opt" %R% REF1)
+      question_numbers <<- str_c("a", activity_no, "_", "q", raw_question_numbers, "_", question_type, maq_option_no, sep = "")  
+    }
+    
+    if (sum(maqV)==0) {
+      question_numbers <<- str_c("a", activity_no, "_", "q", raw_question_numbers, "_", question_type, sep = "")  
+    }
+    
+    column_names[logV] <<- question_numbers
+    colnames(df_list[[df_name]]) <<- column_names   
+  }   
+}
+
+# 6. Was: {r create:  function to add a column with number of missing values in that row}
+add_all_msg_cols <- function() {
+  for (df_name in df_names)  {
+    df_list[[df_name]] <<- df_list[[df_name]] %>% naniar::add_n_miss()  
+  }
+}
+
+# 7. Was: {r create:  arrange by answer dttm and add a column with ordinal no of observation}
+add_all_obs.no_cols <- function() {
+  for (df_name in df_names)  {
+    df_list[[df_name]] <<- df_list[[df_name]] %>% arrange(record_time)
+    activity_no <<- str_extract(df_name, pattern = pattern6) %>% str_remove_all("df") %>% str_remove_all("_")
+    new.var <<- paste("obs", activity_no, sep = "." )
+    assign(new.var, 1:nrow(df_list[[df_name]]))
+    df_list[[df_name]] <<- df_list[[df_name]] %>% mutate("obs.{activity_no}" := eval(sym(new.var)), .before = uuid)
+  }
+}
+
+# 8. Was: {r Create: function to change some var classes}
+change_some_classes <- function() {
+  for (df_name in df_names)  {
+    df_list[[df_name]] <<- df_list[[df_name]] %>% select(all_of(sortvec), starts_with("obs."), everything())
+    
+    df_list[[df_name]] <<- df_list[[df_name]] %>% 
+      mutate(participant_id = as.integer(participant_id)) %>%
+      mutate(participant_status = as.integer(participant_status)) %>%
+      mutate(status = as.integer(status)) %>%
+      mutate(triggering_logic_id = as.integer(triggering_logic_id)) %>%
+      mutate(triggering_logic_type = as.integer(triggering_logic_type))
+  }
+}
+
+# 9. Create a combined df and re-order its columns  
+create_combined_df <- function() {
+  combined_df <<- bind_rows(df_list, .id = "original_df")   
+  
+  combined_df <<- combined_df %>% select(all_of(sortvec), starts_with("obs."), everything()) %>%
+    relocate(c(starts_with("obs"), original_df), .before = uuid)
+}
+
+
+# 10. Was: ```{r Create: extract data frames from list if needed}
+extract_all_df <- function() {
+list2env(df_list, .GlobalEnv)
+}
+
+# . Was: 
 
--- deprecated.R
+++ deprecated.R
@@ -1,5 +1,5 @@
 
-# ```{r Deprecated}
+# ```{r Deprecated} -----
 # # testing
 # x1 <- (df_list[[1]][[1,3]])
 # number_extract_single2((df_list[[1]][[1,3]]))
@@ -49,7 +49,7 @@
 #   }
 
 
-# # test
+# # test -----
 # x4 <- "{\"answer_id\":null,\"answer_content\":\"7\",\"answer_location\":{\"latitude\":null,\"longitude\":null,\"accuracy\":null,\"speed\":null},\"answer_response_time\":\"2024-02-19T03:35:15.692000+00:00\",\"answer_question_content\":\"In general how much of the time do you feel you are making progress towards accomplishing your goals?\\n(If your answer is zero you still have to move the slider back and forth so it knows you are not skipping the question.)\\n0 = Never\\n10 = Always\",\"answer_media_interactions\":[],\"unanswered_status_id\":null}"
 # 
 # str_detect(x4, pattern4)  #   
@@ -59,6 +59,7 @@
 # str_detect(x5, pattern4)  #  
 
 
+# testing regex in funs -----
 # # create function to extract numerical data from 'metadata' column 
 # pattern1 <- "\\{\"answer_id\":" %R% capture(DGT) %R% capture(optional(DGT)) %R% one_or_more(printable())
 # 
@@ -79,11 +80,9 @@
 #   repeated(optional(DGT),0,5)
 
 
-# ***********
-
 # ```
 
-# ```{r Test:    try to rewrite col name regex to include MAQ questions, eval=FALSE}
+# ```{r Test:    try to rewrite col name regex to include MAQ questions, eval=FALSE} ----
 # # current regex
 # pattern3.1 <- "_" %R% repeated("[a-z]", lo = 3, hi = 3) %R% "_metadata_question_" %R% capture(DGT) %R%
 #   repeated(capture(optional(DGT)),0,2) %R% "_of_survey_" %R% (DGT) %R%
@@ -117,7 +116,7 @@
 
 # ```
 
-```{r Create: functions for output cleaning}
+# ```{r Create: functions for output cleaning}
 # # Function to create column with count of NAs     # redundant- can use a fun from naniar:: or tidyr::, so replace 
 # countRowNAs <- function(x) { sum(is.na(x)) }
 # 
@@ -125,7 +124,88 @@
 #   apply(data, 1, countRowNAs)
 # }     
 
-```
+# ```
+
+# ```{r deprecated, eval=FALSE} -----
+# combined_df %<>% 
+#   mutate(participant_id = as.integer(participant_id)) %>% 
+#   mutate(participant_status = as.integer(participant_status)) %>% 
+#   mutate(obs_id = as.integer(obs_id)) %>% 
+#   mutate(status = as.integer(status)) %>% 
+#   mutate(triggering_logic_id = as.integer(triggering_logic_id)) %>% 
+#   mutate(triggering_logic_type = as.integer(triggering_logic_type)) 
+
+# test_df <- data.frame(a = c(6,7,8), b = c("a", "b", "c"))
+# j <- 0
+# for (j in 0:2)  {
+#   j <- j + 1
+# 
+#   varname <- paste("constant_val", j, sep = "." )
+#   assign(varname, 1:nrow(test_df))
+# 
+#   test_df %<>% mutate("constant_val.{j}" := eval(sym(varname)))
+#   }
+
+# for (df_name in df_names)  {
+#   df_list[[df_name]] %<>% arrange(record_time)
+#   activity_no <- str_extract(df_name, pattern = pattern6) %>% str_remove_all("df") %>% str_remove_all("_")
+#   newvar <- paste("obs", activity_no, sep = "." )
+#   df_list[[df_name]] %<>% mutate(newvar = row_number(), .before = uuid)
+#  }
+
+# ```
+
+
+# ```{r Create: functions for preprocessing multiple answer questions, eval=FALSE}
+# # put NA in 1340_Q19_MAQ
+# test_pathways_baselines_df <- test_pathways_baselines_df %>%
+#     mutate_at(vars(`1340_Q19_MAQ`), ~ na_if(., ""))             # NB mutate_at() may be depreceated by nnow; replace
+
+# # create multiple cols for 1340_Q19_MAQ. First use str_split to create a matrix with one col for each answer, then make into df.
+# diagnosed_conditions_mx <- str_split(string = test_pathways_baselines_df$`1340_Q19_MAQ`, pattern = " & ", simplify = TRUE)
+# diagnosed_conditions_df <- as.data.frame(diagnosed_conditions_mx)
+# 
+# # Do I need a join to get the data in the right place? It seems not. 
+# test_pathways_baselines_df <- bind_cols(test_pathways_baselines_df, diagnosed_conditions_df)
+# 
+# # rename columns. May need more code if someone has more than 6 answers. But I don't; the most answers anyone had was 6.  
+# test_pathways_baselines_df <- rename(test_pathways_baselines_df, `1340_Q19_MAQ_1` = V1, `1340_Q19_MAQ_2` = V2, `1340_Q19_MAQ_3` = V3, `1340_Q19_MAQ_4` = V4, `1340_Q19_MAQ_5` = V5, `1340_Q19_MAQ_6` = V6)
+# 
+# # use str_replace to get numbers only, dump characters
+# test_pathways_baselines_df <- test_pathways_baselines_df %>% 
+#     mutate_at(vars(contains("1340_Q19_MAQ_")), number_extract_single)
+# 
+# # create col with concatenation of all Q19_MAQ cols
+# test_pathways_baselines_df <- test_pathways_baselines_df %>% unite(`1340_Q19_MAQ_concat`, `1340_Q19_MAQ_1`:`1340_Q19_MAQ_6`, sep = ",", remove = FALSE)
+# 
+# # create binary cols for each concat answer
+# pattern1 <- or1(c(",1,", ",1" %R% END, START %R% "1,"))
+# pattern2 <- or1(c(",2,", ",2" %R% END, START %R% "2,"))
+# pattern3 <- or1(c(",3,", ",3" %R% END, START %R% "3,"))
+# pattern4 <- or1(c(",4,", ",4" %R% END, START %R% "4,"))
+# pattern5 <- or1(c(",5,", ",5" %R% END, START %R% "5,"))
+# pattern7 <- or1(c(",6,", ",6" %R% END, START %R% "6,"))
+# pattern8 <- or1(c(",7,", ",7" %R% END, START %R% "7,"))
+# pattern8 <- or1(c(",8,", ",8" %R% END, START %R% "8,"))
+# pattern9 <- or1(c(",9,", ",9" %R% END, START %R% "9,"))
+# pattern10 <- or1(c(",10,", ",10" %R% END, START %R% "10,"))
+# 
+# test_pathways_baselines_df <- test_pathways_baselines_df %>% 
+#     mutate(`1340_Q19_MAQ_option1` = ifelse(str_detect(string = `1340_Q19_MAQ_concat`, pattern = pattern1), TRUE, FALSE)) %>%
+#     mutate(`1340_Q19_MAQ_option2` = ifelse(str_detect(string = `1340_Q19_MAQ_concat`, pattern = pattern2), TRUE, FALSE)) %>%
+#     mutate(`1340_Q19_MAQ_option3` = ifelse(str_detect(string = `1340_Q19_MAQ_concat`, pattern = pattern3), TRUE, FALSE)) %>% 
+#      mutate(`1340_Q19_MAQ_option4` = ifelse(str_detect(string = `1340_Q19_MAQ_concat`, pattern = pattern4), TRUE, FALSE)) %>% 
+#      mutate(`1340_Q19_MAQ_option5` = ifelse(str_detect(string = `1340_Q19_MAQ_concat`, pattern = pattern5), TRUE, FALSE)) %>% 
+#      mutate(`1340_Q19_MAQ_option6` = ifelse(str_detect(string = `1340_Q19_MAQ_concat`, pattern = pattern7), TRUE, FALSE)) %>% 
+#     mutate(`1340_Q19_MAQ_option7` = ifelse(str_detect(string = `1340_Q19_MAQ_concat`, pattern = pattern8), TRUE, FALSE)) %>% 
+#     mutate(`1340_Q19_MAQ_option8` = ifelse(str_detect(string = `1340_Q19_MAQ_concat`, pattern = pattern8), TRUE, FALSE)) %>% 
+#     mutate(`1340_Q19_MAQ_option9` = ifelse(str_detect(string = `1340_Q19_MAQ_concat`, pattern = pattern9), TRUE, FALSE)) %>% 
+#     mutate(`1340_Q19_MAQ_option10` = ifelse(str_detect(string = `1340_Q19_MAQ_concat`, pattern = pattern10), TRUE, FALSE))
+# 
+# test_pathways_baselines_df <- test_pathways_baselines_df %>% 
+#     mutate_at(vars(`1340_Q19_MAQ_1`:`1340_Q19_MAQ_6`), ~ factor(., levels = c(1:10), labels = c("Heart disease", "Other heart condition", "Blood clot thrombosis", "Asthma", "Hay fever", "Depression", "Anxiety", "Thyroid problems", "None of these", "Other"), exclude = NA))
+
+# ```
 
 
 
